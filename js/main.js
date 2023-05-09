6b34260cacbe5868453698feb3da5214a4eab4f3
Tatiana Kh <65133778+t-skoro@users.noreply.github.com>
2023-05-09 19:49:02 +0500

    Подключение слайдера

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
-customSelect('select');
+$(document).ready(function () {
+
+    /* --- FANCYBOX --- */
+    Fancybox.bind("[data-fancybox]", {
+        Thumbs: false,
+        // отключение миниатюры
+        Toolbar: {
+            display: {
+                left: [],
+                middle: [],
+                right: ["close"],
+            },
+        },
+    });
+
+    /* --- CUSTOM SELECT --- */
+    customSelect('select');
+
+    /* --- OWLCAROUSEL --- */
+    $('.products__cards').owlCarousel({
+        margin: 24,
+        items: 1,
+        loop: true,
+        smartSpeed: 800,
+        dotsSpeed: 1200,
+        // stagePadding: 40, - отображение соседних карточек
+        center: true,
+        responsive: {
+            400: {
+                items: 2,
+                center: true
+            },
+            768: {
+                items: 3,
+                center: true
+            },
+            1200: {
+                items: 3,
+                center: true
+            }
+        }
+    });
+
+    // обрабатываем клик по кастомным кнопкам-стрелкам
+    $('.arrow-prev').on('click', function () {
+        $('.products__cards').trigger('prev.owl.carousel');
+    });
+
+    $('.arrow-next').on('click', function () {
+        $('.products__cards').trigger('next.owl.carousel');
+    });
+});
